9c6759bd955d4c8f733607012ffac8922568b440
levani-hacker <114279610+levani-hacker@users.noreply.github.com>
2023-10-01 15:53:23 +0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,113 @@
+// const person = {
+//     name: "John",
+//     age: 30,
+//     email: "john@gmail.com",
+//     introduce() {
+//       console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//     },
+//     address: {
+//       street: "123 Main St",
+//       city: "New York",
+//       postalCode: "10001",
+//     },
+//   };
+  
+//   const person1 = { name: "Alice", age: 25, email: "alice@gmail.com" };
+//   const person2 = { name: "Alice", age: 25, email: "alice@gmail.com" };
+
+//   delete person.email;
+
+
+
+
+
+
+// function logProperties(obj) {
+//     for (const key in obj) {
+//       console.log(`${key}: ${obj[key]}`);
+//     }
+//   }
+  
+//   function countProperties(obj) {
+//     const count = Object.keys(obj).length;
+//     console.log(`Number of properties: ${count}`);
+//   }
+  
+//   function logKeys(obj) {
+//     const keys = Object.keys(obj);
+//     console.log("Keys:", keys);
+//   }
+  
+//   function logValues(obj) {
+//     const values = Object.values(obj);
+//     console.log("Values:", values);
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sayHi = function () {
+//     console.log("Hi there!");
+//   };
+  
+//   sayHi();
+
+
+
+
+
+
+// function testScope() {
+//     const x = 10;
+//     console.log(x);
+//   }
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+// const double = (number = 0) => number * 2;
+
+
+
+
+
+
+
+
+
+
+
+
+// const greet = () => "Hello, world!";
+
+
+
+
+// const ArrowFunction = {
+//     sayHello: (name = "Guest") => {
+//       console.log(`Hello, ${name}!`);
+//     },
+//   };
+  
+ 
+  
